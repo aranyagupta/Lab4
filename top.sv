db3d2e85dcde8ab39e0a68a9214c8ba5cd691c60
Advik Chitre <advik.chitre42@gmail.com>
2022-11-18 11:23:54 +0000

    add doit.sh + progress update

---
+++ top.sv
@@ -0,0 +1,12 @@
+module top #(
+	parameter WIDTH = 16
+)(
+  // interface signals
+  input  logic             clk,      // clock 
+  input  logic             rst,      // reset
+  output  logic            a0        // output signal
+);
+
+  // add stuff
+
+endmodule
---
+++ top_tb.cpp
@@ -0,0 +1,63 @@
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vtop.h"
+
+#include "vbuddy.cpp"     // include vbuddy code
+#define MAX_SIM_CYC 1000000
+
+int main(int argc, char **argv, char **env) {
+  int simcyc;     // simulation clock count
+  int tick;       // each clk cycle has two ticks for two edges
+
+  Verilated::commandArgs(argc, argv);
+  // init top verilog instance
+  Vtop* top = new Vtop;
+  // init trace dump
+  Verilated::traceEverOn(true);
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  top->trace (tfp, 99);
+  tfp->open ("top.vcd");
+ 
+  // init Vbuddy
+  if (vbdOpen()!=1) return(-1);
+  vbdHeader("L4: Reduced RISC-V CPU");
+  vbdSetMode(1);        // Flag mode set to one-shot
+
+  // initialize simulation input 
+  top->clk = 1;
+  top->rst = 0;
+  top->en = 1;
+
+
+  // intialize variables for analogue output
+
+  // run simulation for MAX_SIM_CYC clock cycles
+  for (int simcyc=0; simcyc<MAX_SIM_CYC; simcyc++) {
+    printf("\nrst: %d, en: %d, [simcyc: %d] ", top->rst, top->en, simcyc);
+    // dump variables into VCD file and toggle clock
+    for (tick=0; tick<2; tick++) {
+      printf("[tick: %d] ", tick);
+      tfp->dump (2*simcyc+tick);
+      top->clk = !top->clk;
+      top->eval ();
+    }
+    
+    top->rst = 0;
+    if (simcyc < 2) top->rst = 1;
+
+    // plot RAM input/output, send sample to DAC buffer, and print cycle count
+    vbdHex(1, top->a0 & 0xF);
+    vbdBar(top->a0 & 0xFF);
+    vbdCycle(simcyc);
+
+    top->en = 1;//top->en = vbdFlag();
+    // either simulation finished, or 'q' is pressed
+    if ((Verilated::gotFinish()) || (vbdGetkey()=='q')) 
+      exit(0);
+  }
+
+  vbdClose();     // ++++
+  tfp->close(); 
+  printf("Exiting\n");
+  exit(0);
+}
