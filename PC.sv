61bf124b8b85dfbc9e0f274e147a860a31836b52
jessc21 <yiruchen1921@gmail.com>
2022-11-23 17:17:03 +0000

    delet the unnecessary parts in PC.sv

--- PC.sv
+++ PC.sv
@@ -5,17 +5,11 @@ module PC #(
     input logic              clk,         //clock
     input logic              rst,         //reset
     input logic  [WIDTH-1:0] next_PC,     //count input
-    // input logic  [WIDTH-1:0] ImmOp,
-    // inout logic              PCsrc,
     output logic [WIDTH-1:0] PC           //count output
 );
 
 //syncronous reset
 always_ff @ (posedge clk)      //specifies a clocked circuit 
-    // if (rst) begin PC <= {WIDTH{1'b0}};
-    // end else begin  if (PCsrc) PC <= PC + ImmOp; 
-    //                 else PC <= PC + 4;
-    // end
     if (rst)
         PC<={WIDTH{1'b0}};
     else
